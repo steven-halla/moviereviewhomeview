c1cd790fdb2532d2c07ed211aa6c2abe6b777d53
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-01-13 23:57:32 -0800

    "creating carousel"

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import 'core-js/es/symbol'
-import Carousel from 'flat-carousel';
+import styled from "styled-components";
 
-
-const images = [
-  { src: 'some image' }
-];
+const AppDiv = styled.div`
+  
+`
 
 
 export function App() {
   return (
-    <div className="App">
-      <Carousel>
-        {images.map((image, index) => (
-          <div
-            key={index}
-            className="demo-item"
-            style={{ backgroundImage: 'url(' + image.src + ')' }}
-          />
-        ))}
-      </Carousel>
-    </div>
+    <AppDiv>
+
+
+
+
+    </AppDiv>
   );
 }
 
