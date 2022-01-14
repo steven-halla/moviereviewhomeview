a0d2c7d0b58b69109ca4c0bb3a0f21f5679dd4c3
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-01-14 01:21:34 -0800

    "basic styling"

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,25 @@ import React from 'react';
 import styled from "styled-components";
 
 const AppDiv = styled.div`
-  
+  body {
+    margin: 0;
+    padding: 0;
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+    background-color: #1e1e1e;
+  }
 `
 
 
 export function App() {
   return (
     <AppDiv>
+      <body>
 
 
 
-
+      </body>
     </AppDiv>
   );
 }
