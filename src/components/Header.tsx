f0211d5a37f08d2ef84c2c62adc0b03cf6d98bdd
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-01-16 12:53:30 -0800

    "header component finished"

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,15 +5,90 @@ import styled from "styled-components";
 const StyleHeaderDiv = styled.div`
 
   .title {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+   
+    font-size: 1.9em;
+    font-family: "Noto Sans Old Italic";
     height: 70px;
     width: 100%;
-    background-color: red;
+    background: linear-gradient(to right, #cc0404, #d74646);
+    color: white;
   }
   
-  .links {
-    height: 80px;
+  .top-header {
     width: 100%;
-    background-color: skyblue;
+    height: 100px;
+    display: flex;
+    justify-content: center;
+    padding: 0 0;
+    margin: 0 30px 0 0;
+    color: white;
+
+
+
+
+    .links {
+      width: 100%;
+
+      max-width: 800px;
+      background: linear-gradient(to right, #cc0404, #d74646);
+      display: flex;
+      flex-flow: row nowrap;
+      //justify-content: flex-start;
+      justify-content: space-evenly;
+      align-items: center;
+      padding: 20px 10px;
+      margin-bottom: -15px;
+      color: white;
+
+      p {
+
+        font-size: 50px;
+        padding-bottom: 25px;
+        flex-direction: column;
+      }
+
+
+      .link {
+
+        height: 100%;
+        line-height: 50px; // set line-height = height of container (50px) and this will vertically center text.
+        white-space: nowrap;
+        color: white;
+
+
+        &:hover {
+          border-bottom: 2px solid black;
+        }
+
+        // do this for bigger screens
+        //&:last-child {
+        //  margin-left: auto;
+        //  text-align: right;
+        //  text-wrap: none;
+        //}
+      }
+
+      .selected {
+        border-bottom: 2px solid black;
+      }
+    }
+
+    a,
+    a:visited
+    {
+      font-size: 1.4em;
+      text-decoration: none;
+      color: white;
+    }
+    a:hover {
+      font-size: 1.8em;
+      color: black;
+    }
+
+
   }
 `;
 
@@ -25,11 +100,14 @@ export const Header: FC = (props) => {
       <div className="title">
         <p>Tom Rotten</p>
       </div>
-      <div className="links">
-        <a href="">Movies</a>
-        <a href="">Critics</a>
-        <a href="">Home</a>
-        <a href="">Profile</a>
+      <div className="top-header">
+        <div className="links">
+          <a href="">Movies</a>
+          <a href="">Critics</a>
+          <a href="">Home</a>
+          <a href="">Profile</a>
+        </div>
+
       </div>
 
 
@@ -60,77 +138,77 @@ export const Header: FC = (props) => {
 
 //
 // const StyleHeaderDiv = styled.div`
-//   &.top-header {
-//     width: 100%;
-//     height: 100px;
-//     display: flex;
-//     justify-content: center;
-//     padding: 0 0;
-//     margin: 0 30px 0 0;
-//     color: white;
-//
-//
-//
-//
-//     .links {
-//       width: 100%;
-//
-//       max-width: 800px;
-//       background: linear-gradient(to right, #cc0404, #d74646);
-//       display: flex;
-//       flex-flow: row nowrap;
-//       //justify-content: flex-start;
-//       justify-content: space-evenly;
-//       align-items: center;
-//       padding: 20px 10px;
-//       margin-bottom: -15px;
-//       color: white;
-//
-//       p {
-//
-//         font-size: 50px;
-//         padding-bottom: 25px;
-//         flex-direction: column;
-//       }
-//
-//
-//       .link {
-//
-//         height: 100%;
-//         line-height: 50px; // set line-height = height of container (50px) and this will vertically center text.
-//         white-space: nowrap;
-//         color: white;
-//
-//
-//         &:hover {
-//           border-bottom: 2px solid black;
-//         }
-//
-//         // do this for bigger screens
-//         //&:last-child {
-//         //  margin-left: auto;
-//         //  text-align: right;
-//         //  text-wrap: none;
-//         //}
-//       }
-//
-//       .selected {
-//         border-bottom: 2px solid black;
-//       }
-//     }
-//
-//     a,
-//     a:visited
-//  {
-//       font-size: 1.4em;
-//       text-decoration: none;
-//       color: white;
-//     }
-//     a:hover {
-//       font-size: 1.8em;
-//       color: black;
-//     }
-//
-//
-//   }
+////  &.top-header {
+////    width: 100%;
+////    height: 100px;
+////    display: flex;
+////    justify-content: center;
+////    padding: 0 0;
+////    margin: 0 30px 0 0;
+////    color: white;
+////
+////
+////
+////
+////    .links {
+////      width: 100%;
+////
+////      max-width: 800px;
+////      background: linear-gradient(to right, #cc0404, #d74646);
+////      display: flex;
+////      flex-flow: row nowrap;
+////      //justify-content: flex-start;
+////      justify-content: space-evenly;
+////      align-items: center;
+////      padding: 20px 10px;
+////      margin-bottom: -15px;
+////      color: white;
+////
+////      p {
+////
+////        font-size: 50px;
+////        padding-bottom: 25px;
+////        flex-direction: column;
+////      }
+////
+////
+////      .link {
+////
+////        height: 100%;
+////        line-height: 50px; // set line-height = height of container (50px) and this will vertically center text.
+////        white-space: nowrap;
+////        color: white;
+////
+////
+////        &:hover {
+////          border-bottom: 2px solid black;
+////        }
+////
+////        // do this for bigger screens
+////        //&:last-child {
+////        //  margin-left: auto;
+////        //  text-align: right;
+////        //  text-wrap: none;
+////        //}
+////      }
+////
+////      .selected {
+////        border-bottom: 2px solid black;
+////      }
+////    }
+////
+////    a,
+////    a:visited
+//// {
+////      font-size: 1.4em;
+////      text-decoration: none;
+////      color: white;
+////    }
+////    a:hover {
+////      font-size: 1.8em;
+////      color: black;
+////    }
+////
+////
+////  }
 // `;
