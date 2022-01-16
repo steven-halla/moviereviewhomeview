b18c86b3f61e83244ecbb6954fbd78d3e805c0c0
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-01-15 17:17:23 -0800

    "setting up slides component"

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SliderData} from "./SliderData";
+import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from 'react-icons/fa'
 
 export const ImageSlider = () => {
+  const [current, setCurrent] = useState(0)
+  const length = slides.length
+
+
+
   return (
-    <div>
-      {SliderData.map((slide, index) => {
+    <div className="slider">
+      <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
+      <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />
+
+        {SliderData.map((slide, index) => {
          return <img src={slide.image} alt="aliens"/>
 
       })};
