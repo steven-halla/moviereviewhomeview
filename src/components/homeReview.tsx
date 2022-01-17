4630ea2a2ce3a63cb82da40dd43798ab244c80b9
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-01-17 10:05:21 -0800

    "creating review div"

--- src/components/homeReview.tsx
+++ src/components/homeReview.tsx
@@ -3,14 +3,21 @@ import React, {FC} from 'react';
 import styled from "styled-components";
 
 const StyleHeaderDiv = styled.div`
-  
+  .tom-rotten-review {
+    background-color: grey;
+  }
 `
 
 export const HomeReview: FC = (props) => {
 
 return (
   <StyleHeaderDiv>
-    <p>Hi</p>
+    <p>Movie reviews in 150 characters or less</p>
+    <p>This is what a tom rotten review looks like:</p>
+    <div className="tom-rotten-review">
+      <p>"One review takes up about 150 characters. This means your reviews will be short. At about 3 sentences and no more."</p>
+    </div>
+
 
 
   </StyleHeaderDiv>
