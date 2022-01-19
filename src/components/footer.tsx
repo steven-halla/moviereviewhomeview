72ceabe3e8d231c2fa0b6d9c9f99b64e5bda4f22
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-01-18 17:13:32 -0800

    "fixing image size on social media links"

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -15,6 +15,11 @@ const StyleFooterDiv = styled.div`
     height: 155px;
     width: 222px;
     background-color: lightgray;
+    
+    img {
+      height: 30px;
+      width: auto;
+    }
   }
   
   `
@@ -26,7 +31,19 @@ export const Footer: FC = (props) => {
 
           <div className="Footer-Container">
               <div className="Social-Media-Container">
-
+                <h2>Like what you see? Connect with us on Social media!</h2>
+                <p><a href="https://www.facebook.com/" target="_blank"><img
+                  src="https://cdn1.iconfinder.com/data/icons/logotypes/32/square-facebook-512.png" height="60"
+                  width="60" alt=""/></a></p>
+                <p><a href="https://twitter.com/" target="_blank"><img
+                  src="https://d1e2bohyu2u2w9.cloudfront.net/education/sites/default/files/product/twitter-product-image.png"
+                  height="60" width="60" alt=""/></a></p>
+                <p><a href="https://www.linkedin.com/" target="_blank"><img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS4s3k1pIajgk3Rf-07CLW-WqbfKXE2VDXFA&usqp=CAU&ec=45761791"
+                  height="60" width="60" alt=""/></a></p>
+                <p><a href="https://www.pinterest.com/" target="_blank"><img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEgxnwMTT4WrNoXdjQlJZE4Vxj1Gnd8v1OGg&usqp=CAU&ec=45761791"
+                  height="60" width="60" alt=""/></a></p>
               </div>
           </div>
 
