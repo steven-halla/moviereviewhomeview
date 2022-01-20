d445d0d2024f251197f22fccf9c96e7ae621d3fe
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-01-19 20:37:02 -0800

    "refactor"

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -87,13 +87,10 @@ const StyleHeaderDiv = styled.div`
       font-size: 1.8em;
       color: black;
     }
-
-
   }
 `;
 
 export const Header: FC = (props) => {
-
 
   return (
     <StyleHeaderDiv>
@@ -117,98 +114,4 @@ export const Header: FC = (props) => {
 }
 
 
-// export const Header: FC  = (props) => {
-//
-//
-//   return (
-//     <StyleHeaderDiv className="top-header">
-//
-//       <div className="links">
-//
-//         <a href="">Movies</a>
-//         <a href="">Critics</a>
-//         <a href="">Home</a>
-//         <a href="">Profile</a>
-//       </div>
-//
-//     </StyleHeaderDiv>
-//   );
-//
-// }
 
-//
-// const StyleHeaderDiv = styled.div`
-////  &.top-header {
-////    width: 100%;
-////    height: 100px;
-////    display: flex;
-////    justify-content: center;
-////    padding: 0 0;
-////    margin: 0 30px 0 0;
-////    color: white;
-////
-////
-////
-////
-////    .links {
-////      width: 100%;
-////
-////      max-width: 800px;
-////      background: linear-gradient(to right, #cc0404, #d74646);
-////      display: flex;
-////      flex-flow: row nowrap;
-////      //justify-content: flex-start;
-////      justify-content: space-evenly;
-////      align-items: center;
-////      padding: 20px 10px;
-////      margin-bottom: -15px;
-////      color: white;
-////
-////      p {
-////
-////        font-size: 50px;
-////        padding-bottom: 25px;
-////        flex-direction: column;
-////      }
-////
-////
-////      .link {
-////
-////        height: 100%;
-////        line-height: 50px; // set line-height = height of container (50px) and this will vertically center text.
-////        white-space: nowrap;
-////        color: white;
-////
-////
-////        &:hover {
-////          border-bottom: 2px solid black;
-////        }
-////
-////        // do this for bigger screens
-////        //&:last-child {
-////        //  margin-left: auto;
-////        //  text-align: right;
-////        //  text-wrap: none;
-////        //}
-////      }
-////
-////      .selected {
-////        border-bottom: 2px solid black;
-////      }
-////    }
-////
-////    a,
-////    a:visited
-//// {
-////      font-size: 1.4em;
-////      text-decoration: none;
-////      color: white;
-////    }
-////    a:hover {
-////      font-size: 1.8em;
-////      color: black;
-////    }
-////
-////
-////  }
-// `;
